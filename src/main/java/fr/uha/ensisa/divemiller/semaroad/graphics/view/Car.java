4be30d0d6ac5ddfc4b42c5e0bd374c20f9a5aa1d
Guillaume-Ecole <guillaume_dev@Guillaume.localdomain>
2023-01-15 18:24:13 +0100

    if inutil

--- src/main/java/fr/uha/ensisa/divemiller/semaroad/graphics/view/Car.java
+++ src/main/java/fr/uha/ensisa/divemiller/semaroad/graphics/view/Car.java
@@ -43,15 +43,11 @@ public class Car implements DynamicGraphicObject {
             case WAITING:
                 switch (lane) {
                     case LEFT:
-                        if (position.getX() < nextPosition.getX()) {
-                        } else {
-                            if (!arrivedEndPosition) {
+                        if (position.getX() > nextPosition.getX())
+                            if (!arrivedEndPosition)
                                 this.releaseArrivedEndPosition();
-                            }
-                            arrivedEndPosition = true;
-                        }
+                        arrivedEndPosition = true;
                         break;
-
                     default:
                         break;
                 }
